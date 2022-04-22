2bb68f6bd695cb6d4091e6065df543ed0c99cd26
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-04-21 20:20:44 -0400

    Add calculator logic

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,29 +1,33 @@
 import { PureComponent } from 'react';
 import '../Calculator.css';
-import PropTypes from 'prop-types';
 import Button from './button';
 
 export default class Calculator extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-
     };
   }
 
+  componentDidMount() {
+    this.setState(() => ({
+      result: 'Please enter some numbers to begin!',
+    }));
+  }
+
   render() {
-    const { result } = this.props;
+    const { result } = this.state;
     return (
       <div className="Calculator">
         <input type="text" className="display" name="result" placeholder="0" id="result" value={result} />
-        <Button className="clear" value="AC" />
-        <Button className="neg" value="+/-" />
+        <Button className="number clear" value="AC" />
+        <Button className="number neg" value="+/-" />
         <Button className="number percent" value="%" />
-        <Button className="number divide" value="÷" />
-        <Button className="number seven" value="7" />
+        <Button className="divide" value="÷" />
+        <Button className="seven" value="7" />
         <Button className="number eight" value="8" />
         <Button className="number nine" value="9" />
-        <Button className="number multiply" value="x" />
+        <Button className="multiply" value="x" />
         <Button className="number four" value="4" />
         <Button className="number five" value="5" />
         <Button className="number six" value="6" />
@@ -31,19 +35,11 @@ export default class Calculator extends PureComponent {
         <Button className="number one" value="1" />
         <Button className="number two" value="2" />
         <Button className="number three" value="3" />
-        <Button className="number add" value="+" />
+        <Button className="add" value="+" />
         <Button className="number zero" value="0" />
-        <Button className="decimal" value="." />
-        <Button className="number equals" value="=" />
+        <Button className="number decimal" value="." />
+        <Button className="equals" value="=" />
       </div>
     );
   }
 }
-
-Calculator.propTypes = {
-  result: PropTypes.string.isRequired,
-};
-
-Calculator.defaultPropTypes = {
-  result: '0',
-};
